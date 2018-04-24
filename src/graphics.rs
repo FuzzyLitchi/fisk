6ee1907f341b49204bd5bd493cc2cd94726b991f
Emile Yvan Boutet Livoff <emilelivoff@gmail.com>
2018-04-24 10:10:07 +0200

    Move graphics into a new graphics module

---
+++ src/graphics.rs
@@ -0,0 +1,58 @@
+use std::mem;
+use libc;
+
+use blit::{BlitExt, Color};
+
+use image;
+use image::DynamicImage;
+
+pub const WIDTH: usize = 512;
+pub const HEIGHT: usize = 512;
+
+pub struct Graphics {
+    images: Vec<DynamicImage>,
+    pub buffer: Vec<u32>,
+}
+
+impl Graphics {
+    pub fn new() -> Self {
+        Self {
+            images: Vec::new(),
+            buffer: vec![0x00FFFFFF; WIDTH * HEIGHT],
+        }
+    }
+
+    pub fn new_image(&mut self, path: &str) -> usize {
+        self.images.push(image::open(path).unwrap());
+        self.images.len() - 1
+    }
+
+    pub fn draw_image(&mut self, id: usize, x: i32, y: i32) {
+        let image = &self.images[id];
+
+        match image.as_rgba8() {
+            Some(rgba) => rgba.blit(
+                    &mut self.buffer,
+                    WIDTH,
+                    (x, y),
+                    Color::from_u32(0xFF00FF)
+                ),
+            None => image.as_rgb8().unwrap().blit(
+                    &mut self.buffer,
+                    WIDTH,
+                    (x, y),
+                    Color::from_u32(0xFF00FF)
+                )
+        };
+    }
+
+    pub fn clear(&mut self) {
+        unsafe {
+            libc::memset(
+                self.buffer.as_mut_ptr() as _,
+                0x00FFFFFF,
+                self.buffer.len() * mem::size_of::<u32>(),
+            );
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,31 @@
-use std::collections::HashMap;
-use std::mem;
-
 extern crate libc;
-
 extern crate blit;
-use blit::{BlitExt, Color};
+extern crate image;
 
 extern crate minifb;
 use minifb::{Window, WindowOptions, Key};
 
-extern crate image;
-use image::DynamicImage;
-
-extern crate rand;
-
-const WIDTH: usize = 512;
-const HEIGHT: usize = 512;
+mod graphics;
+use graphics::Graphics;
 
 struct Fisk {
-    images: HashMap<u64, DynamicImage>,
-    buffer: Vec<u32>,
-    test: u64,
-    smiley: u64,
+    graphics: Graphics,
+    test: usize,
+    smiley: usize,
 }
 
 impl Fisk {
     fn new() -> Self {
-        Fisk {
-            images: HashMap::new(),
-            buffer: vec![0x00FFFFFF; WIDTH * HEIGHT],
+        Self {
+            graphics: Graphics::new(),
             test: 0,
             smiley: 0,
         }
     }
 
-    fn new_image(&mut self, path: &str) -> u64 {
-        let id = rand::random::<u64>();
-        self.images.insert(id, image::open(path).unwrap());
-        id
-    }
-
-    fn draw_image(&mut self, id: u64, x: i32, y: i32) {
-        let image = self.images.get(&id).unwrap();
-
-        match image.as_rgba8() {
-            Some(rgba) => rgba.blit(
-                    &mut self.buffer,
-                    WIDTH,
-                    (x, y),
-                    Color::from_u32(0xFF00FF)
-                ),
-            None => image.as_rgb8().unwrap().blit(
-                    &mut self.buffer,
-                    WIDTH,
-                    (x, y),
-                    Color::from_u32(0xFF00FF)
-                )
-        };
-    }
-
     fn load(&mut self) {
-        self.test = self.new_image("test.png");
-        self.smiley = self.new_image("smiley.png");
+        self.test = self.graphics.new_image("test.png");
+        self.smiley = self.graphics.new_image("smiley.png");
     }
 
     fn update(&mut self) {
@@ -69,29 +33,26 @@ impl Fisk {
     }
 
     fn draw(&mut self) {
-        unsafe {
-            libc::memset(
-                self.buffer.as_mut_ptr() as _,
-                0x00FFFFFF,
-                self.buffer.len() * mem::size_of::<u32>(),
-            );
-        }
+        //Clear the screen
+        self.graphics.clear();
+
+        //Tmp test stuff
         let id = self.test;
-        self.draw_image(id, 0, 0);
+        self.graphics.draw_image(id, 0, 0);
         let id = self.smiley;
-        self.draw_image(id, 0, 0);
+        self.graphics.draw_image(id, 0, 0);
     }
 }
 
 pub fn main() {
     let mut fisk = Fisk::new();
 
-    let mut window = Window::new("fisk", WIDTH, HEIGHT, WindowOptions::default()).expect("Unable to open window");
+    let mut window = Window::new("fisk", graphics::WIDTH, graphics::HEIGHT, WindowOptions::default()).expect("Unable to open window");
 
     fisk.load();
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
+    while window.is_open() {
         fisk.draw();
-        window.update_with_buffer(&fisk.buffer).unwrap();
+        window.update_with_buffer(&fisk.graphics.buffer).unwrap();
     }
 }
